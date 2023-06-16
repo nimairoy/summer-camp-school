48bb0cc06e6f2b11a0c958cf67e32d7b67f5901a
nimairoy <nimairoy718@gmail.com>
2023-06-17 01:22:39 +0600

    explore axios interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,37 +1,38 @@
-import {  useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import useAuth from './useAuth';
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+import axios from "axios";
+import { useEffect } from "react";
 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'http://localhost:5000',
 });
 
 const useAxiosSecure = () => {
-    const {logout} = useAuth();
-    const navigate = useNavigate();    
+
+    const {signout} = useAuth();
+    const navigate = useNavigate();
 
     useEffect(()=>{
-        axiosSecure.interceptors.request.use( config => {
-            const token = localStorage.getItem('access_token');
+        axiosSecure.interceptors.request.use(config => {
+            const token = localStorage.getItem('access-token');
             if(token){
-                config.headers.authorization = `Bearer ${token}`;
+                config.headers.Authorization = `Bearer ${token}`;
             }
             return config;
-        })
+        });
 
-        axiosSecure.interceptors.response.use(response => response, async(error) => {
+        axiosSecure.interceptors.response.use(response => response, async(error)=>{
             if(error.response && (error.response.status === 401 || error.response.status === 403)){
-               await logout();
-               navigate('/login')
+                await signout();
+                navigate('/login');
             }
-            return Promise.reject(error)
+            return Promise.reject(error);
         })
 
-    }, [logout, navigate])
-    
-    return [axiosSecure];
+    }, [signout, navigate ])
+
+    return [ axiosSecure ];
 };
 
 export default useAxiosSecure;
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,13 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
-    const {user} = useAuth();
-    const {isLoading, refetch, data: cart = []} = useQuery({
+    const { user, loading } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const { isLoading, refetch, data: cart = [] } = useQuery({
         queryKey: ['cart', user?.email],
-        queryFn: async() => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
-            return res.json();
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure(`/carts?email=${user?.email}`);
+            console.log('res from axios inside', res)
+            return res.data;
         }
     })
     return [cart, isLoading, refetch];
