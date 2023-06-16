cf08c268b20f4a9e8d94ec3f3fba8bc29eec9c2c
nimairoy <nimairoy718@gmail.com>
2023-06-16 16:21:44 +0600

    all users page created

--- src/pages/Dashboard/AllUser/AllUser.jsx
+++ src/pages/Dashboard/AllUser/AllUser.jsx
@@ -30,13 +30,15 @@ const AllUser = () => {
 
     return (
         <div className="overflow-x-auto w-3/4">
+            <h2 className="text-3xl text-center mb-6">All Users Here</h2>
             <table className="table w-full">
                 <thead>
                     <tr>
                         <th>#</th>
                         <th>Name</th>
                         <th>Email</th>
-                        <th>Role</th>
+                        <th>Instructor</th>
+                        <th>Admin</th>
                         <th>Action</th>
                     </tr>
                 </thead>
@@ -48,7 +50,8 @@ const AllUser = () => {
                             <th>{index + 1}</th>
                             <td>{user.name}</td>
                             <td>{user.email}</td>
-                            <td>{user.role === 'admin' ? 'admin' : <FaUserShield onClick={() => handleMakeAdmin(user._id)} className='text-4xl p-2 rounded bg-yellow-400 text-yellow-800 cursor-pointer'></FaUserShield>}</td>
+                            <td>{user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user._id)} className='px-4 font-semibold py-2 rounded bg-yellow-400 text-yellow-800'>Make Instructor</button>}</td>
+                            <td>{user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user._id)} className='px-4 font-semibold py-2 rounded bg-yellow-400 text-yellow-800'>Make Admin</button>}</td>
                             <td><AiFillDelete onClick={() => handleDelete(user._id)} className='text-4xl p-2 rounded bg-red-400 text-red-800 cursor-pointer'></AiFillDelete></td>
                         </tr>)
                     }
